e3ae3b0eb54f1e05c6799994d073a146e56127d5
Irina Pankratova <pairvale@gmail.com>
2019-08-15 17:29:38 +0300

    ***

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const searchForm = document.querySelector('#search-form');
 const movie = document.querySelector('#movies');
 
-function apiSearch(evt) {
+// function apiSearch(evt) {
+//     evt.preventDefault();
+//     const searchText = document.querySelector('.form-control').value;
+//     const server = 'https://api.themoviedb.org/3/search/multi?api_key=618653b3a45b6be44c46f38f077a1d0b&language=ru&query=' + searchText;
+//     requestApi('GET', server);
+// }
+
+searchForm.addEventListener('submit', function(evt) {
     evt.preventDefault();
     const searchText = document.querySelector('.form-control').value;
     const server = 'https://api.themoviedb.org/3/search/multi?api_key=618653b3a45b6be44c46f38f077a1d0b&language=ru&query=' + searchText;
     requestApi('GET', server);
-}
-
-searchForm.addEventListener('submit', apiSearch);
+});
 
 function requestApi(method, url) {
     
