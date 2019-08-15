17e916d05c11b58567fba50c0d2e3afd898315cd
Irina Pankratova <pairvale@gmail.com>
2019-08-15 10:17:03 +0300

    implemented movies title output

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const searchForm = document.querySelector('#search-form');
+const movie = document.querySelector('#movies');
 
 function apiSearch(evt) {
     evt.preventDefault();
     const searchText = document.querySelector('.form-control').value;
     const server = 'https://api.themoviedb.org/3/search/multi?api_key=618653b3a45b6be44c46f38f077a1d0b&language=ru&query=' + searchText;
-
     requestApi('GET', server);
 }
 
@@ -22,9 +22,20 @@ function requestApi(method, url) {
         }
         
         if (request.status !== 200) {
-            console.log('Error: ' +request.status);
+            console.log('Error: ' + request.status);
             return;
         }
-        console.log(request.readyState);
+
+        const output = JSON.parse(request.responseText);
+
+        let inner = '';
+
+        output.results.forEach(function(item){
+            let itemName = item.name || item.title;
+            console.log(itemName);
+            inner += '<div class="col-3"> ' + itemName + ' </div>';
+        });
+
+        movie.innerHTML = inner;
     });
 }
