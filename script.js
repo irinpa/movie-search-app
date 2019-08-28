1692a15c377658bf0ace70c96a01e1669fecc43b
Irina Pankratova <pairvale@gmail.com>
2019-08-28 16:27:47 +0300

    Movie poster display

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const searchForm = document.querySelector('#search-form');
 const movie = document.querySelector('#movies');
+const urlPoster = 'https://image.tmdb.org/t/p/w185/';
+const emptyPosterUrl = 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg';
 
 function apiSearch(evt) {
     evt.preventDefault();
@@ -10,21 +12,40 @@ function apiSearch(evt) {
     fetch(server)
         .then( 
             response => {
+                console.log('response.status:' +response.status);
+                if (response.status !== 200) {
+                    return Promise.reject(new Error('from fetch: ' + response.status));
+                }
                 return response.json();
             })
         .then(
             result => {
+                console.log(result);
                 let inner = '';
                 for (let item of result.results){
                     let itemName = item.name || item.title;
-                    inner += `<div class="col-12 col-md-4 col-xl-3">${itemName}</div>`;
+                    if (item.poster_path) {
+                        inner += `
+                        <div class="col-12 col-md-4 col-xl-3 item">
+                        <img src = "${urlPoster + item.poster_path}" alt="${itemName}">
+                        <h5>${itemName}</h5>
+                        </div>`;
+                    } else {
+                        inner += `
+                        <div class="col-12 col-md-4 col-xl-3 item">
+                        <img src = "${emptyPosterUrl}" alt="${itemName}">
+                        <h5>${itemName}</h5>
+                        </div>`;
+                    }
+                     
+                    
                 }
             movie.innerHTML = inner;
             })
         .catch( 
             reason => {
                 movie.innerHTML = 'OOPS... Something went wrong...';
-                console.log('Error: ' + reason.status);
+                console.log('Error in catch: ' + reason);
         });
 }    
 
