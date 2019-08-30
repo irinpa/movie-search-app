b8436d557678b6eebdbfad737d135ab2af5aa3a5
Irina Pankratova <pairvale@gmail.com>
2019-08-30 17:49:11 +0300

    DOMContentLoaded

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const searchForm = document.querySelector('#search-form');
 const movie = document.querySelector('#movies');
 const urlPoster = 'https://image.tmdb.org/t/p/w185/';
-const emptyPosterUrl = 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg';
 
 function apiSearch(evt) {
     evt.preventDefault();
@@ -17,7 +16,7 @@ function apiSearch(evt) {
     fetch(server)
         .then( 
             response => {
-                console.log('response.status:' +response.status);
+                console.log(response.status);
                 if (response.status !== 200) {
                     return Promise.reject(new Error('from fetch: ' + response.status));
                 }
@@ -25,7 +24,7 @@ function apiSearch(evt) {
             })
         .then(
             result => {
-                console.log(result);
+                // console.log(result);
                 let inner = '';
                 if(result.results.length === 0){
                     inner = '<h5 class="col-12 text-center text-info">По вашему запросу ничего не найдено</h5>';
@@ -59,13 +58,14 @@ function apiSearch(evt) {
         .catch( 
             reason => {
                 movie.innerHTML = 'OOPS... Something went wrong...';
-                console.log('Error in catch: ' + reason);
+                console.log('Error in catch: ' + reason || reason.status);
         });
 }    
 
 searchForm.addEventListener('submit', apiSearch);
 
 function addEventMedia() {
+    // зачем data-id?
     const media = movie.querySelectorAll('img[data-id]');
 
     for (let elem of media){
@@ -75,5 +75,65 @@ function addEventMedia() {
 }
 
 function showFullInfo(){
-    console.log(this);
+    let url = '';
+    if(this.dataset.type === 'movie'){
+        url = 'кино';
+    } else if (this.dataset.type === 'tv'){
+        url = 'сериал';
+    } else {
+        movie.innerHTML = '<h5 class="col-12 text-center text-danger">Произошла ошибка, повторите позже</h5>';
+    }
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+    fetch('https://api.themoviedb.org/3/trending/all/week?api_key=618653b3a45b6be44c46f38f077a1d0b&language=ru')
+    .then( 
+        response => {
+            console.log('response.status:' +response.status);
+            if (response.status !== 200) {
+                return Promise.reject(new Error('from fetch: ' + response.status));
+            }
+            return response.json();
+        })
+    .then(
+        result => {
+            console.log(result);
+
+            let inner = '<h5 class="col-12 text-center text-info">Популярное за неделю</h5>';
+
+            if(result.results.length === 0){
+                inner = '<h5 class="col-12 text-center text-info">По вашему запросу ничего не найдено</h5>';
+            }
+
+            for (let item of result.results){
+                // console.log(item);
+                let itemName = item.name || item.title;
+                let mediaType = item.title ? 'movie' : 'tv';
+                const poster = item.poster_path ? urlPoster + item.poster_path : './img/noposter.png';
+                
+                let dataInfo = `data-id="${item.id}" data-type="${mediaType}"`;
+
+                // if (item.media_type !== 'person'){
+
+                //     dataInfo = `data-id="${item.id}"
+                //     data-type="${item.media_type}"`;
+                // }
+
+                inner += `
+                <div class="col-12 col-md-6 col-xl-3 item">
+                <img src = "${poster}" class = "poster" alt="${itemName}" ${dataInfo}>
+                <h5>${itemName}</h5>
+                </div>
+                `;
+            }    
+        movie.innerHTML = inner;
+
+        addEventMedia();
+
+        })
+    .catch( 
+        reason => {
+            movie.innerHTML = 'OOPS... Something went wrong...';
+            console.log('Error in catch: ' + reason || reason.status);
+    });
+});
