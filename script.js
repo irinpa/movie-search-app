8f5367d49b930ace8e22a52a78405e3fc3295b90
Irina Pankratova <pairvale@gmail.com>
2019-09-09 17:44:22 +0300

    add youtube movie previews

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function apiSearch(evt) {
         movie.innerHTML = '<h5 class="col-12 text-center text-danger">Поле поиска не должно быть пустым</h5>';
         return;
     }
-    const server = 'https://api.themoviedb.org/3/search/multi?api_key=618653b3a45b6be44c46f38f077a1d0b&language=ru&query=' + searchText;
+    // const server = 'https://api.themoviedb.org/3/search/multi?api_key=618653b3a45b6be44c46f38f077a1d0b&language=ru&query=' + searchText;
     movie.innerHTML = '<div class="spinner"></div>';
 
-    fetch(server)
+    fetch('https://api.themoviedb.org/3/search/multi?api_key=618653b3a45b6be44c46f38f077a1d0b&language=ru&query=' + searchText)
         .then( 
             response => {
                 console.log(response.status);
@@ -31,7 +31,7 @@ function apiSearch(evt) {
                 }
 
                 for (let item of result.results){
-                    console.log(item);
+                    // console.log(item);
                     let itemName = item.name || item.title;
                     const poster = item.poster_path ? urlPoster + item.poster_path : './img/noposter.png';
                     
@@ -123,67 +123,106 @@ function showFullInfo(){
 
                         <p> Описание: ${result.overview} </p>
 
+                        <br>
 
-                    </div>
-                `;
+                        <div class="youtube"></div>
 
-            })
+
+                    </div>`;
+                    getVideo(this.dataset.type, this.dataset.id);
+            }
+        )
+
+                
+
         .catch( 
             reason => {
-                movie.innerHTML = 'OOPS... Something went wrong...';
+                movie.innerHTML = 'Упс! Что-то пошло не так :(';
                 console.log('Error in catch: ' + reason || reason.status);
         });
 }
 
 document.addEventListener('DOMContentLoaded', function(){
     fetch('https://api.themoviedb.org/3/trending/all/week?api_key=618653b3a45b6be44c46f38f077a1d0b&language=ru')
-    .then( 
-        response => {
-            console.log('response.status:' +response.status);
-            if (response.status !== 200) {
-                return Promise.reject(new Error('from fetch: ' + response.status));
+        .then( 
+            response => {
+                console.log('response.status:' +response.status);
+                if (response.status !== 200) {
+                    return Promise.reject(new Error('from fetch: ' + response.status));
+                }
+                return response.json();
+            })
+        .then(
+            result => {
+                console.log(result);
+    
+                let inner = '<h5 class="col-12 text-center text-info">Популярное за неделю</h5>';
+    
+                if(result.results.length === 0){
+                    inner = '<h5 class="col-12 text-center text-info">По вашему запросу ничего не найдено</h5>';
+                }
+    
+                for (let item of result.results){
+                    // console.log(item);
+                    let itemName = item.name || item.title;
+                    let mediaType = item.title ? 'movie' : 'tv';
+                    const poster = item.poster_path ? urlPoster + item.poster_path : './img/noposter.png';
+                    
+                    let dataInfo = `data-id="${item.id}" data-type="${mediaType}"`;
+    
+                    inner += `<div class="col-12 col-md-6 col-xl-3 item">
+                    <img src = "${poster}" class = "poster" alt="${itemName}" ${dataInfo}>
+                    <h5>${itemName}</h5>
+                    </div>`;
+                }    
+
+                movie.innerHTML = inner;
+    
+                addEventMedia();
             }
-            return response.json();
+        )
+
+        .catch( 
+            reason => {
+                movie.innerHTML = 'Упс! Что-то пошло не так :(';
+                console.log('Error in catch: ' + reason || reason.status);
+        });
+});    
+
+function getVideo(type, id) {
+    let youtube = movie.querySelector('.youtube');
+
+    fetch(`https://api.themoviedb.org/3/${type}/${id}/videos?api_key=618653b3a45b6be44c46f38f077a1d0b&language=ru`)
+        .then( 
+            response => {
+                if (response.status !== 200) {
+                    return Promise.reject(new Error('from fetch: ' + response.status));
+                }
+                return response.json();
         })
-    .then(
-        result => {
-            console.log(result);
+        .then(
+            result => {
+                console.log(result);
+                let videoFrame = '<h5 class="text-info">Трейлеры</h5>';
 
-            let inner = '<h5 class="col-12 text-center text-info">Популярное за неделю</h5>';
-
-            if(result.results.length === 0){
-                inner = '<h5 class="col-12 text-center text-info">По вашему запросу ничего не найдено</h5>';
-            }
-
-            for (let item of result.results){
-                // console.log(item);
-                let itemName = item.name || item.title;
-                let mediaType = item.title ? 'movie' : 'tv';
-                const poster = item.poster_path ? urlPoster + item.poster_path : './img/noposter.png';
+                if (result.results.length === 0){
+                    videoFrame = '<p>Трейлеры отсутствуют</p>';
+                }
                 
-                let dataInfo = `data-id="${item.id}" data-type="${mediaType}"`;
+                for (let item of result.results){
+                    videoFrame += `<iframe width="560" height="315" src="https://www.youtube.com/embed/${item.key}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+                    
+                }
+                youtube.innerHTML = videoFrame;
+            }
+        )
+        .catch(
+            reason => {
+                youtube.innerHTML = 'Трейлера не существует';
+                console.error('Error in catch: ' + reason || reason.status);
+            }
+        );
 
-                // if (item.media_type !== 'person'){
 
-                //     dataInfo = `data-id="${item.id}"
-                //     data-type="${item.media_type}"`;
-                // }
-
-                inner += `
-                <div class="col-12 col-md-6 col-xl-3 item">
-                <img src = "${poster}" class = "poster" alt="${itemName}" ${dataInfo}>
-                <h5>${itemName}</h5>
-                </div>
-                `;
-            }    
-        movie.innerHTML = inner;
-
-        addEventMedia();
-
-        })
-    .catch( 
-        reason => {
-            movie.innerHTML = 'OOPS... Something went wrong...';
-            console.log('Error in catch: ' + reason || reason.status);
-    });
-});
+    youtube.innerHTML = id;
+}
